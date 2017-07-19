ba445efae52ab589780caf688b8a582a588484d3
andreasbehnke <ab@andreasbehnke.com>
2017-07-19 21:01:53 +0200

    implemented hardening script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # routeros-hardening
-Contains all commands described at https://www.manitonetworks.com/mikrotik/2016/5/24/mikrotik-router-hardening#neighbor-discovery
+Harding script based on information from these websites:
+
+* https://www.manitonetworks.com/mikrotik/2016/5/24/mikrotik-router-hardening#neighbor-discovery
+* https://kieranjones.uk/web-filtering-on-mikrotik-routerboard/
--- hardening.sh
+++ hardening.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 
+# https://www.manitonetworks.com/mikrotik/2016/5/24/mikrotik-router-hardening#neighbor-discovery
+
 # SSH
 
 # - use very strong password
 # - use public key for authentication
 # - use strong crypto
-
-PASSWORD=$(pwgen -s -y 21 1)
-scp ~/.ssh/id_rsa.pub  admin@192.168.88.1:
+ROUTERIP="192.168.88.1"
+PASSWORD=$(pwgen -s -y 20 1)
+echo $PASSWORD
+scp ~/.ssh/id_rsa.pub  admin@$ROUTERIP:
 sleep 1
-ssh admin@192.168.88.1 'user ssh-keys import user=admin public-key-file=id_rsa.pub'
-ssh admin@192.168.88.1 "user set 0 password=\"$PASSWORD\""
-ssh admin@192.168.88.1 'ip ssh set strong-crypto=yes'
+ssh admin@$ROUTERIP 'user ssh-keys import user=admin public-key-file=id_rsa.pub'
+ssh admin@$ROUTERIP "user set 0 password=\"$PASSWORD\""
+ssh admin@$ROUTERIP 'ip ssh set strong-crypto=yes'
+
+# disable unused services
+
+ssh admin@$ROUTERIP 'ip service disable [find name=telnet]'
+ssh admin@$ROUTERIP 'ip service disable [find name=ftp]'
+ssh admin@$ROUTERIP 'ip service disable [find name=www]'
+ssh admin@$ROUTERIP 'ip service disable [find name=www-ssl]'
+ssh admin@$ROUTERIP 'ip service disable [find name=api]'
+ssh admin@$ROUTERIP 'ip service disable [find name=api-ssl]'
+ssh admin@$ROUTERIP 'tool bandwidth-server set enabled=no'
+ssh admin@$ROUTERIP 'ip dns set allow-remote-requests=no'
+ssh admin@$ROUTERIP 'ip socks set enabled=no'
+
+# disable MAC services for finding devices
+
+ssh admin@$ROUTERIP 'tool mac-server set [find] disabled=yes'
+ssh admin@$ROUTERIP 'tool mac-server mac-winbox set [find] disabled=yes'
+ssh admin@$ROUTERIP 'tool mac-server ping set enabled=no'
+
+# disable monitoring tool
+
+ssh admin@$ROUTERIP 'tool romon set enabled=no'
+
+# internet filter (https://kieranjones.uk/web-filtering-on-mikrotik-routerboard/)
+
+ssh admin@$ROUTERIP 'ip dns set allow-remote-requests=yes'
+ssh admin@$ROUTERIP 'ip dns set servers=199.85.126.20,199.85.127.20'
+ssh admin@$ROUTERIP 'ip firewall filter add chain=input comment="TCP DNS" connection-state=new dst-port=53 protocol=tcp src-address=192.168.88.1/24'
+ssh admin@$ROUTERIP 'ip firewall filter add chain=input comment="UDP DNS" connection-state=new dst-port=53 protocol=udp src-address=192.168.88.1/24'
